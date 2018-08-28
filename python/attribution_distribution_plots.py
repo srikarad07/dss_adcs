9e6855d5746d636a730fce6951b63622377b6e78
srikarad07 <s.d.cherukuri@student.tudelft.nl>
2018-08-28 09:11:12 +0200

    Add a plotting function for product attributes.

---
+++ python/attribution_distribution_plots.py
@@ -0,0 +1,54 @@
+'''
+Copyright (c) 2017,S.D Cherukuri, Delft University of Technology (srikarad007@gmail.com)
+Distributed under the MIT License.
+See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+'''
+
+# Set up modules and packages.
+# Plotting
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Numerical
+import numpy as np
+import pandas as pd
+
+# System
+# import sys
+import time
+import os 
+
+# Locate the string 
+from csv_functions import stringLocator
+from csv_functions import requiredFiles
+
+plt.style.use('seaborn-whitegrid')
+
+print ""
+print "------------------------------------------------------------------"
+print "                             dss_adcs                             "
+print "                              0.1.0                               "
+print "      Copyright (c) 2018, S.D. Cherukuri (srikarad007@gmail.com)  "
+print "------------------------------------------------------------------"
+print ""
+
+# Start timer.
+start_time = time.time( )
+
+print ""
+print "******************************************************************"
+print "                          Input parameters                        "
+print "******************************************************************"
+print ""
+
+pathToFile      = "/home/superman/Desktop/productAttributesData/metadata.csv"
+metadata        = pd.read_csv( pathToFile )
+
+n_bins = 20
+
+mass            = metadata['mass']
+fig, axs        = plt.subplots( 1, 1, tight_layout=True )
+
+n, bins, patches = axs.hist( mass, bins=n_bins)
+
+plt.show()
