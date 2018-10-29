544df8c8934fbf79b7025f594188f5307e23d6c1
srikarad07 <s.d.cherukuri@student.tudelft.nl>
2018-10-29 16:52:14 +0100

    Add test for the new style of the json parser.

--- test/testGetReactionWheel.cpp
+++ test/testGetReactionWheel.cpp
@@ -1,37 +1,37 @@
-// /*
-// * Copyright (c) 2018, S.D. Cherukuri, Delft University of Technology (srikarad007@gmail.com)
-// * Distributed under the MIT License.
-// * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
-// */
+/*
+* Copyright (c) 2018, S.D. Cherukuri, Delft University of Technology (srikarad007@gmail.com)
+* Distributed under the MIT License.
+* See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+*/
 
-// #include <cmath>
-// #include <iostream>
-// #include <experimental/filesystem>
+#include <cmath>
+#include <iostream>
+#include <experimental/filesystem>
 
-// #include <catch.hpp>
+#include <catch.hpp>
 
-// #include "dss_adcs/apiCall.hpp"
-// #include "dss_adcs/getReactionWheel.hpp"
-// #include "dss_adcs/tools.hpp"
+#include "dss_adcs/apiCall.hpp"
+#include "dss_adcs/getReactionWheel.hpp"
+#include "dss_adcs/tools.hpp"
 
-// namespace dss_adcs
-// {
-// namespace tests
-// {
-// //! getReactionWheel consists of: callTheApi & getReactionWheelAttributes. For the test,
-// //! these units will be tested first and the then getReactionWheel as a whole. 
+namespace dss_adcs
+{
+namespace tests
+{
+//! getReactionWheel consists of: callTheApi & getReactionWheelAttributes. For the test,
+//! these units will be tested first and the then getReactionWheel as a whole. 
 
-// //! Test call the API
-// /* The product string is generated using system level cURL. If the test 
-// *  test fails the cURL script should be run again in the test directory to 
-// *  to update the product strings.
-// */ 
+//! Test call the API
+/* The product string is generated using system level cURL. If the test 
+*  test fails the cURL script should be run again in the test directory to 
+*  to update the product strings.
+*/ 
 
-// //! Curl system command for testing the api (replace 'productUuid' with the actual Uuid in the url below.)
-// // curl -G https://api.satsearch.co/v1/products/productUuid -H "Authorization: Bearer 0405ab03e7ba20b906b9f154fced17a4271ba92230e64c2e7cec5e30f9a8b660a0de6b1339ddbe3d2e45eb0ee9847c4alu8+41RxSWVaJpqS8XLxdqKqOC0y75jJOraWB0jYvlUIJgvmqEQhIVUGypazLrNO" -H "X-APP-ID: 18a15813704205b3cf2ebeb364aa2ed246a9ed143b6911de4a52cb6045a0049ad1c951a6e8943e3dbc8c5c2c4242a447AbJtXh3J6aazkWKpCRz3nMut6Wu1YCWoEQESOin6qvkeqeDshqw0onjlA7dfM+3n" -o product.json --libcurl product.c
+//! Curl system command for testing the api (replace 'productUuid' with the actual Uuid in the url below.)
+// curl -G https://api.satsearch.co/v1/products/productUuid -H "Authorization: Bearer 0405ab03e7ba20b906b9f154fced17a4271ba92230e64c2e7cec5e30f9a8b660a0de6b1339ddbe3d2e45eb0ee9847c4alu8+41RxSWVaJpqS8XLxdqKqOC0y75jJOraWB0jYvlUIJgvmqEQhIVUGypazLrNO" -H "X-APP-ID: 18a15813704205b3cf2ebeb364aa2ed246a9ed143b6911de4a52cb6045a0049ad1c951a6e8943e3dbc8c5c2c4242a447AbJtXh3J6aazkWKpCRz3nMut6Wu1YCWoEQESOin6qvkeqeDshqw0onjlA7dfM+3n" -o product.json --libcurl product.c
 
-// // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TO DO: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //  
-// // Automate the generation of reaction wheel string from within the function itself //  
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TO DO: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //  
+// Automate the generation of reaction wheel string from within the function itself //  
 
 // TEST_CASE( "Test 1: Test the function callTheApi", "[MR 103D thruster]")
 // {
@@ -48,20 +48,20 @@
 //     REQUIRE( reactionWheelDataStringComputed.compare(reactionWheelDataStringExpected) == 0);     
 // } 
 
-// TEST_CASE( "Test 2: Test the function callTheApi", "[Micro-wheel 4000 Uuid]")
-// {
-//     const std::string actuatorUuid      = "6454b937-f0e9-5fc5-8165-0cc79c44b424";
+TEST_CASE( "Test 2: Test the function callTheApi", "[Micro-wheel 4000 Uuid]")
+{
+    const std::string actuatorUuid      = "6454b937-f0e9-5fc5-8165-0cc79c44b424";
     
-//     const std::string reactionWheelDataStringComputed = callTheApi( actuatorUuid );
+    const std::string reactionWheelDataStringComputed = callTheApi( actuatorUuid );
 
-//     // Generate the reaction wheel data string using cURL. 
-//     std::string currentPath     = std::experimental::filesystem::current_path(); 
-//     std::ifstream inputFile(currentPath + "/../test/testProductJson/product2.json");
+    // Generate the reaction wheel data string using cURL. 
+    std::string currentPath     = std::experimental::filesystem::current_path(); 
+    std::ifstream inputFile(currentPath + "/../test/testProductJson/product2.json");
 
-//     const std::string reactionWheelDataStringExpected    = getStringFromFile(inputFile); 
+    const std::string reactionWheelDataStringExpected    = getStringFromFile(inputFile); 
 
-//     REQUIRE( reactionWheelDataStringComputed.compare(reactionWheelDataStringExpected) == 0);     
-// } 
+    REQUIRE( reactionWheelDataStringComputed.compare(reactionWheelDataStringExpected) == 0);     
+} 
 
 // TEST_CASE( "Test 3: Test the function callTheApi", "[rw-0-01 Sinclair Interplanetary Uuid]")
 // {
@@ -82,33 +82,40 @@
 // //! in satsearch. The conversions from units given in the datasheets to SI units is done before 
 // //! verifying with the values obtained from the getReactionWheelAttribute function. 
 
-// TEST_CASE( "Test 4: Test the getReactionWheelAttributes function", "[Microwheel 4000]")
-// {
-//     // Tolerance to compensate for machine precision. 
-//     const Real tolerance = 1e-12; 
+TEST_CASE( "Test 4: Test the getReactionWheelAttributes function", "[Microwheel 4000]")
+{
+    // Tolerance to compensate for machine precision. 
+    const Real tolerance = 1e-12; 
 
-//     // Use the reaction wheel string from the previous tests from product2.json // 
-//     const std::string currentPath     = std::experimental::filesystem::current_path(); 
-//     std::ifstream inputFile(currentPath + "/../test/testProductJson/product2.json");
-//     const std::string reactionWheelDataString     = getStringFromFile(inputFile); 
+    // Use the reaction wheel string from the previous tests from product2.json // 
+    const std::string currentPath     = std::experimental::filesystem::current_path(); 
+    std::ifstream inputFile(currentPath + "/../test/testProductJson/product2.json");
+    const std::string reactionWheelDataString     = getStringFromFile(inputFile); 
 
-//     // Extract the properties of microwheel 4000 with the string from the getReactionWheelAttributes
-//     const ReactionWheel computedReactionWheel     = getReactionWheelAttributes( reactionWheelDataString ); 
+    // Expected acutator uuid. 
+    const std::string expectedActuatorUuid      = "6454b937-f0e9-5fc5-8165-0cc79c44b424";
 
-//     // Define a reaction wheel with the properties of Microwheel 4000 from satsearch datasheet. 
-//     const ReactionWheel expectedReactionWheel( 3.30, 0.218, 0.081, 0.218, std::nan("radius"), 0.300, 4.0, "MicroWheel 4000", "Microsat Systems Canada"); 
-
-//     REQUIRE( computedReactionWheel.mass         == Approx( expectedReactionWheel.mass).epsilon(tolerance) ); 
-//     REQUIRE( computedReactionWheel.length       == Approx( expectedReactionWheel.length).epsilon(tolerance) );
-//     REQUIRE( computedReactionWheel.width        == Approx( expectedReactionWheel.width).epsilon(tolerance) );
-//     REQUIRE( computedReactionWheel.height       == Approx( expectedReactionWheel.height).epsilon(tolerance) );
-//     // REQUIRE( computedReactionWheel.radius       == Approx( expectedReactionWheel.radius).epsilon(tolerance) );
-//     REQUIRE( computedReactionWheel.maxTorque    == Approx( expectedReactionWheel.maxTorque).epsilon(tolerance) );
+    // Extract the properties of microwheel 4000 with the string from the getReactionWheelAttributes
+    const ReactionWheel computedReactionWheel     = getReactionWheelAttributes( reactionWheelDataString, expectedActuatorUuid ); 
     
-//     REQUIRE( computedReactionWheel.name.compare( expectedReactionWheel.name )                   == 0);
-//     REQUIRE( computedReactionWheel.supplierName.compare( expectedReactionWheel.supplierName )   == 0);
+    // Define a reaction wheel with the properties of Microwheel 4000 from satsearch datasheet. 
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<< TO DO >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> // 
+    // Random value of power is taken here and hence is not tested!! TEST THAT!! 
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<< TO DO >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> // 
+
+    const ReactionWheel expectedReactionWheel( 3.30, 0.218, 0.081, 0.218, std::nan("radius"), 0.300, 4.0, 1.0, "MicroWheel 4000", "Microsat Systems Canada", expectedActuatorUuid); 
+
+    REQUIRE( computedReactionWheel.mass         == Approx( expectedReactionWheel.mass).epsilon(tolerance) ); 
+    REQUIRE( computedReactionWheel.length       == Approx( expectedReactionWheel.length).epsilon(tolerance) );
+    REQUIRE( computedReactionWheel.width        == Approx( expectedReactionWheel.width).epsilon(tolerance) );
+    REQUIRE( computedReactionWheel.height       == Approx( expectedReactionWheel.height).epsilon(tolerance) );
+    // REQUIRE( computedReactionWheel.radius       == Approx( expectedReactionWheel.radius).epsilon(tolerance) );
+    REQUIRE( computedReactionWheel.maxTorque    == Approx( expectedReactionWheel.maxTorque).epsilon(tolerance) );
+    
+    REQUIRE( computedReactionWheel.name.compare( expectedReactionWheel.name )                   == 0);
+    REQUIRE( computedReactionWheel.supplierName.compare( expectedReactionWheel.supplierName )   == 0);
         
-// }
+}
 
 // TEST_CASE( "Test 5: Test the getReactionWheelAttributes function", "[rw-0-01 Sinclair Interplanetary Uuid]")
 // {
@@ -221,7 +228,7 @@
 
 // }
 
-// } // tests 
+} // tests 
 
-// } // dss_adcs
+} // dss_adcs
 
